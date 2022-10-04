11b25d075459c32c32f942d199ddde905576c1ed
stephen shadrach <stephenlgz@gmail.com>
2022-10-04 09:07:04 -0400

    axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { WEATHER_APP_APT_KEY } from "../API_KEYS";
 
 function Home() {
   return (
     <div>
       <h1>Weather App</h1>
+      <p>{WEATHER_APP_APT_KEY}</p>
     </div>
   );
 }
