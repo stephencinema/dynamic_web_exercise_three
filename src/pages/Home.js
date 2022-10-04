d10385e0ec1b7b2957df8f8127f2345cd5d98882
stephen shadrach <stephenlgz@gmail.com>
2022-10-04 09:45:21 -0400

    orlando humidity data test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { WEATHER_APP_APT_KEY } from "../API_KEYS";
 
+// URL for accessing OpenWeatherAPI with Query Params
+const openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=${WEATHER_APP_APT_KEY}`;
+
 function Home() {
+  // Value stored in state for weather data
+  const [weatherData, setWeatherData] = useState({});
+
+  useEffect(() => {
+    // Query OpenWeatherAPT for weather data
+    // make request to OpenWeather
+    axios
+      .get(openWeatherURL)
+      .then(function (response) {
+        setWeatherData(response.data);
+      })
+      .catch(function (error) {
+        console.warn(error);
+        setWeatherData({});
+      });
+  }, []);
+
+  // Display this weather data in our app
+  console.log("state value", weatherData);
+
   return (
     <div>
       <h1>Weather App</h1>
-      <p>{WEATHER_APP_APT_KEY}</p>
+      <h2>{weatherData.name}</h2>
+      <p>Humidity: {weatherData.main && weatherData.main.humidity}%</p>
     </div>
   );
 }
